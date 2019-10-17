7f7443947912cf285081f9805a386fdcc9673966
Lee <624061283@qq.com>
2019-10-17 15:59:28 +0800

    hash

---
+++ Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Animal
+    {
+        protected int age = 10;
+    }
+    class Person : Animal
+    {
+        public void Shout()
+        {
+            Animal animal = new Animal() as Person;
+            //Person person = new Person() as Animal;
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Father.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Father
+    {
+        protected int age = 2;
+        public void Do()
+        {
+            Father father = new Father();
+            Console.WriteLine(father.age);//2
+            Console.ReadKey();
+        }
+    }
+    class Child : Father
+    {
+        public void Do2()
+        {
+            Console.WriteLine(this.age);//2
+            Child child = new Child();
+            Console.WriteLine(child.age);//2
+            //Father father = new Father();
+            //Console.WriteLine(father.age);//报错
+            Console.ReadKey();
+        }
+    }
+}
